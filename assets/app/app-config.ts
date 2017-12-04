f1a2350c39268a3c090714327a9518472a8f9bb0
Jerome Hurley <vonswirl@googlemail.com>
2017-12-04 11:12:31 +0000

    Update app-config.ts

--- assets/app/app-config.ts
+++ assets/app/app-config.ts
@@ -1,6 +1,6 @@
 import { Headers} from "@angular/http";
 
 export const appConfig = {
-    apiUrl: "http://localhost:3000/as/api/",
+    apiUrl: "http://3amigosa.azurewebsites.net/as/api/",
     header: new Headers({'Content-Type': 'application/json'})
 }
