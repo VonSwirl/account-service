ac7ed1e6c7c63cc5d6de5e2574cd0df09c0c141d
Jerome Hurley <vonswirl@googlemail.com>
2017-12-04 12:55:11 +0000

    Update app.service.ts

--- assets/app/app.service.ts
+++ assets/app/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import 'rxjs/add/operator/map';
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 
